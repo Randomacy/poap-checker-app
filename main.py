cbb0ffe4e5b9dcbf70261587056b2d3e3193564a
Jackie <jackietanyen@gmail.com>
2023-06-08 17:50:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ import pandas as pd
 import numpy as np
 from io import StringIO
 import requests
+from datetime import date
 
 # Declare variables
+headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Mobile Safari/537.36'}
+api_endpoint = "https://frontend.poap.tech/actions/claim-qr?qr_hash="
+link_list = []
+validity_list = []
 
 # Show title of the app
 st.title('POAP Link Checker')
@@ -25,6 +30,42 @@ if uploaded_file:
 
 if uploaded_file:
     if st.button("Check my POAPs' validity!"):
-        print("Hello")
+        progress_text = "Operation in progress. Please wait."        
+        st.write("Checking POAPs now")
+        for line_num in range(len(lines)):
+            if 'app' in lines[line_num]:
+                temp_hash = lines[line_num].replace("https://app.poap.xyz/claim/", "")
+            else:
+                temp_hash = lines[line_num].replace("http://POAP.xyz/claim/", "")
+            
+            temp_endpoint = api_endpoint + temp_hash
+            temp_res = requests.get(temp_endpoint,
+                                    headers = headers)
+            temp_json = temp_res.json()
+            
+            try:
+                if temp_json['claimed']:
+                    st.write(lines[line_num], "is claimed")
+                    validity_list.append('CLAIMED')
+                else:
+                    st.write(lines[line_num], "is still ok")
+                    validity_list.append('NOT CLAIMED')
+            except:
+                validity_list.append('ERROR')
+
+        df = pd.DataFrame({"POAP_URL": lines, 
+                           "Claimed?": validity_list})
+
+        csv = df.to_csv(index = None).encode('utf-8')
+        
+        st.download_button(
+                        label="Download data as CSV",
+                        data=csv,
+                        file_name='checked_poap_links.csv',
+                        mime='text/csv',
+                        )
+
     else:
-        print("Validity Check Ready")
+        print(date.today(), "Validity Check Ready")
+
+
